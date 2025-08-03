47164f199d097be37451d3d157006932f54966ad
bhanu-prakash333 <billabhanuprakash@gmail.com>
2025-08-03 17:07:50 +0530

    bcrypt module added

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ Mongo Error:", err));
 
